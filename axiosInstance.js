90e22f7e5838ebb4b7d44cb7b94147443922d324
keeng arthur <121835767+arthurkeeng@users.noreply.github.com>
2025-02-17 00:27:07 +0100

    Update axiosInstance.js

--- axiosInstance.js
+++ axiosInstance.js
@@ -2,8 +2,8 @@
 
 import axios from 'axios'
 const app = axios.create({
-    // baseURL: 'https://olearningcenter.onrender.com',
-    baseURL: 'http://localhost:4000',
+    
+    baseURL: 'https://nftserver-m3nr.onrender.com',
 
 })
 
